26819e2f7d2ad57fefcd348deecfd8b210a2655e
khoatm <khoatm@rikkeisoft.com>
2023-04-28 15:38:41 +0700

    first commit

---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = '';
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-2.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'dev-uqu7mgnlxwkclkx8.us.auth0.com',
+  clientId: 'dVIbNT9HbnGx8L7HL8hxlMWGN2urx4MI',
+  callbackUrl: 'http://localhost:3000/callback'
+};
